7b40b69f6e1a92b8227337496c7fd16bb313a9e3
Lucas Vinicius Santos Silva <91337081+Lucasvss90@users.noreply.github.com>
2022-05-30 19:28:40 -0300

    Encontro Presencial 4
     Encontro Presencial 4

---
+++ Atividade_jquery/js/javascript.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".nome").css({ "color": "#f00"});  
+    $(".e-mail").css({ "color": "#00f", "font-size": "20px"});
+    $(".celular").css({ "color": "#0f0", "font-size": "40px",  "text-align": "center"});   
+});
